1cbb1cd6c412ae3231b69ee0a73c65a02ded33d6
anil khandei <anil.khandei@hotmail.com>
2024-12-22 18:47:47 -0500

    delete command handler implemented

---
+++ FinEx.Application/Incomes/Commands/DeleteIncomeCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using FinEx.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinEx.Application.Incomes.Commands;
+
+public class DeleteIncomeCommandHandler
+{
+    private readonly IIncomeRepository _incomeRepository;
+
+    public DeleteIncomeCommandHandler(IIncomeRepository incomeRepository)
+    {
+        _incomeRepository = incomeRepository;
+    }
+
+    public async Task Handle(DeleteIncomeCommand command)
+    {
+        await _incomeRepository.DeleteAsync(command.Id);
+    }
+
+}
